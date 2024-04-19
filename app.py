d6eb9aa30c6fae82eb04a6ff2c5b4d74062b0d98
Sai Sudhane <saidon7321@gmail.com>
2024-04-19 16:14:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import google.generativeai as genai
+import streamlit as st
+from streamlit import set_page_config
+
+set_page_config(page_title="Data Science Assistant", page_icon="💻", layout="wide")
+
+st.markdown(
+    "<h1 style='text-align: center; color: blue;'>Data Science Assistant🤖</h1>",
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    "<h3 style='text-align: center; color: grey;'>I'm here to help you grow as a Data Scientist!</h3>",
+    unsafe_allow_html=True,
+)
+
+
+f = open("keys/.gemini_pro_key.txt")
+key = f.read()
+
+genai.configure(api_key=key)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Message Data Science Tutor..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        model = genai.GenerativeModel(
+            model_name="models/gemini-1.5-pro-latest",
+            system_instruction="""Consider yourself as a data scientist. Help the users by answering their queries related to data science!""",
+        )
+        stream = model.generate_content(prompt)
+        response = st.write(stream.text)
+    st.session_state.messages.append({"role": "assistant", "content": stream.text})
